a1e7d08819735c441e8e485df1bd3c0ded316951
Yangfeng Ji <jiyfeng@gmail.com>
2024-05-31 12:00:01 -0400

    test pythia model

--- lora/__init__.py
+++ lora/__init__.py
@@ -4,5 +4,6 @@ from .falcon_lora import Falcon_Lora
 from .flan_t5_lora import Flan_T5_Lora
 from .mistral_lora import Mistral_Lora
 from .gemma_lora import Gemma_Lora
+from .pythia_lora import Pythia_Lora
 
 
